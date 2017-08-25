74bee606cdfccade17b218d20326806dbfbc73f3
Nick Smith <ARMIGER1@users.noreply.github.com>
2017-08-25 07:33:54 -0700

    Add default sector size

--- lib/ramdisk.rb
+++ lib/ramdisk.rb
@@ -1,5 +1,10 @@
 require "ramdisk/version"
+require "ramdisk/mac"
 
 module Ramdisk
-  # Your code goes here...
+  DEFAULT_SECTOR_SIZE = 512
+
+  def self.sector_size(size = DEFAULT_SECTOR_SIZE)
+    size
+  end
 end
--- spec/ramdisk_spec.rb
+++ spec/ramdisk_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Ramdisk do
     expect(Ramdisk::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a default sector size" do
+    expect(Ramdisk::DEFAULT_SECTOR_SIZE).to eq(512)
   end
 end
