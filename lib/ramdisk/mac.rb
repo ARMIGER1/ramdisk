855195d74d1c78c0a9256d01722e085695bf3235
Nick Smith <ARMIGER1@users.noreply.github.com>
2017-08-25 07:45:59 -0700

    Ensure Ramdisk::Mac has a default disk name

--- lib/ramdisk/mac.rb
+++ lib/ramdisk/mac.rb
@@ -1,5 +1,11 @@
 module Ramdisk
   class Mac
+    attr_accessor :disk_name
+
+    def initialize(disk_name: "Untitled")
+      @disk_name = disk_name
+    end
+
     def self.sector_size
       Ramdisk.sector_size
     end
--- spec/mac_spec.rb
+++ spec/mac_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Ramdisk::Mac do
   it "has a default sector size" do
     expect(Ramdisk::Mac.sector_size).to eq(512)
   end
+
+  it "has a default name" do
+    ramdisk = Ramdisk::Mac.new
+
+    expect(ramdisk.disk_name).to eq("Untitled")
+  end
 end
